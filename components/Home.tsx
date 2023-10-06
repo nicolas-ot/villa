96e40218916710ad12201826ec49322decb82f24
Nicolas <nicolastheodarus@gmail.com>
2023-10-06 16:19:28 +0200

    add title for home page

--- components/Home.tsx
+++ components/Home.tsx
@@ -1,7 +1,11 @@
 export default function Home() {
     return (
-        <h1>
-            Luxury Villas for Your Vacation
-        </h1>
+        <div className={"w-full"}>
+            <div className={"w-full mt-28"}>
+                <h1 className={"mx-auto text-7xl w-[48rem] text-center"}>
+                    Luxury Villas for Your Vacation
+                </h1>
+            </div>
+        </div>
     );
 }
